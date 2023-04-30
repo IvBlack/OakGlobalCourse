7b152c9f6f7a1cb453c5eac2de3e5603a52da64b
Ivan Rebrischev <ivbl21@ya.ru>
2023-04-30 20:42:01 +0300

    ClassInitializing added

---
+++ src/main/java/com/IVdev/Core1/PrimitiveTypes/ClassInitializing.java
@@ -0,0 +1,48 @@
+package com.IVdev.Core1.PrimitiveTypes;
+
+/*
+В общем случае инициализация классов в Java проходит так:
+- инициализация стат.блоков
+- переменных стат.блоков
+- инициализация анонимных блоков
+- инициализация констуктора
+*/
+
+public class ClassInitializing {
+    public static void main(String[] args) {
+
+        //инициализируем класс А и стат.блок последовательно
+        //результат виден в консоли
+        A a = new A();
+//        System.out.println(A.st);
+
+
+    }
+}
+
+class A {
+    String strA;
+    int intA;
+
+    public A() {
+        System.out.println("Constructor A.");
+    }
+
+    {
+        System.out.println("Anonymous block is called!");
+        System.out.println("strA = " + strA);
+        System.out.println("intA = " + intA);
+        strA = "default";
+        intA = 2;
+
+        System.out.println("strA = " + strA);
+        System.out.println("intA = " + intA);
+    }
+
+    static String st = "static field";
+    static {
+        System.out.println("Static block is called!");
+        st = "new static field";
+        System.out.println(st);
+    }
+}
