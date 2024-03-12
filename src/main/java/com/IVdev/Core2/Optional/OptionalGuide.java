b5d75cb50cc51b63cd9568a92a8d181767238dc3
Ivan R. <ivbl21@yandex.ru>
2024-03-12 18:14:58 +0300

    sql l.2 + optional edited

--- src/main/java/com/IVdev/Core2/Optional/OptionalGuide.java
+++ src/main/java/com/IVdev/Core2/Optional/OptionalGuide.java
@@ -1,42 +1,48 @@
 package com.IVdev.Core2.Optional;
 
+import java.util.Optional;
+
+
 /*
 Можно воспринимать Optional, как некую коробку, обертку, в которую кладется какой-либо объект.
 Optional всего лишь контейнер: он может содержать объект некоторого типа <Т>, а может быть пустым
 */
-
-import java.util.Optional;
-
 //Сучай работы с репозиторием данных
 public class OptionalGuide {
-    private static PersonRepository personRepository;
+    public static void main(String[] args) {
+        System.out.println("//--------------//Spirit of Optional-----------");
+        Optional<String> empty = Optional.ofNullable(null);
+        System.out.println(empty.isPresent());
+        System.out.println(empty.isEmpty());
 
-    public OptionalGuide(PersonRepository personRepository) {
-        this.personRepository = personRepository;
+
+        System.out.println("//--------------//ifPresent-----------");
+        empty
+            .map(String::toUpperCase)
+            .ifPresent(System.out::println);
+
+        empty.ifPresentOrElse(System.out::println, () -> System.out.println("word"));
     }
 
-    //найти пользователя в базе данных по идентификатору, вывести на консоль длину имени пользователя
-//    final Optional<Person> optPerson = personRepository.findById(1L);
-//    if (optPerson.isPresent()) {
-//        final Person person = optPerson.get();
-//        final String firstName = person.getFirstName();
-//        if (firstName != null) {
-//            System.out.println("Длина твоего имени: " + firstName.length());
+    //без использования Optional-----------------------------
+//    public void calculatePerson() {
+//        PersonRepository personRepository = null;
+//        final Person person = personRepository.findById(1L);
+//        if (person != null) {
+//            final String firstName = person.getFirstName();
+//            if (firstName != null) {
+//                System.out.println("Длина твоего имени: " + firstName.length());
+//            }
+//        }
+
+        //c Optional------------------------
+//        final Optional<Person> optPerson = Optional.ofNullable(personRepository.findById(1L));
+//        if (optPerson.isPresent()) {
+//            final Person person2 = optPerson.get();
+//            final String firstName = person2.getFirstName();
+//            if (firstName != null) {
+//                System.out.println("Длина твоего имени: " + firstName.length());
+//            }
 //        }
 //    }
-
-    public static void main(String[] args) {
-        OptionalGuide optionalGuide = new OptionalGuide(personRepository);
-        optionalGuide.calculatePerson();
-    }
-
-    public void calculatePerson() {
-        final Person person = personRepository.findById(1L);
-        if (person != null) {
-            final String firstName = person.getFirstName();
-            if (firstName != null) {
-                System.out.println("Длина твоего имени: " + firstName.length());
-            }
-        }
-    }
 }
