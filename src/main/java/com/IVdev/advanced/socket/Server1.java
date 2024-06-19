23ddc985b4df822152957207151bd8cb7a036b9c
ivan <ivbl21@yandex.ru>
2024-06-19 22:48:38 +0300

    socket base init

---
+++ src/main/java/com/IVdev/advanced/socket/Server1.java
@@ -0,0 +1,39 @@
+package com.IVdev.advanced.socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+    java.net.Socket представляет собой сокет, который клиент и сервер используют для связи друг с другом.
+    Клиент получает объект сокета, создав его, тогда как сервер получает объект сокета из возвращаемого значения
+    метода accept (). Класс Socket имеет пять конструкторов, которые клиент использует для подключения к серверу.
+*/
+public class Server1 {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(8080))
+        {
+            System.out.println("Server initialized...");
+            while(true) {
+                try
+                (
+                    Socket socket = serverSocket.accept();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                )
+                {
+                    System.out.println("Client connected...");
+                    String request = reader.readLine(); //read the request
+                    String response = String.format("Your request is: %s", request);
+                    writer.write(response);
+                    writer.newLine();
+                    writer.flush(); //clear buffer
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
